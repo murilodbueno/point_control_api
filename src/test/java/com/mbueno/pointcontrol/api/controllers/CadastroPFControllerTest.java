91dfe2c60bf89185b3ba1fba8adc93b41a7bae59
Murilo Diego Bueno <murilodiego1959@gmail.com>
2018-08-22 02:28:08 -0300

    Adding test for CadastroPFController

--- src/test/java/com/mbueno/pointcontrol/api/controllers/CadastroPFControllerTest.java
+++ src/test/java/com/mbueno/pointcontrol/api/controllers/CadastroPFControllerTest.java
@@ -2,7 +2,6 @@ package com.mbueno.pointcontrol.api.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mbueno.pointcontrol.api.PointControlApplication;
 import com.mbueno.pointcontrol.api.dtos.CadastroPFDto;
 import com.mbueno.pointcontrol.api.entities.Empresa;
 import com.mbueno.pointcontrol.api.entities.Funcionario;
@@ -11,27 +10,21 @@ import com.mbueno.pointcontrol.api.services.FuncionarioService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.math.BigDecimal;
 import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
